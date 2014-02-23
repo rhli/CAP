37318cbb584532824782b36c8c011554b386605a
Runhui Li <rhli@proj25>
2014-02-23 19:37:43 +0800

    regular update

--- src/trafficManager.cc
+++ src/trafficManager.cc
@@ -46,8 +46,8 @@ void trafficManager::writeOp(int* loc){
     fprintf(stdout,"write op: starts at %lf",startTime);
     for(int i=0;i<_ecK;i++){
         //TODO: add write into system
-        _nodeTree->dataTransfer(loc[0],loc[1],_blockSize);
-        _nodeTree->dataTransfer(loc[1],loc[2],_blockSize);
+        _nodeTree->dataTransfer(loc[1],loc[0],_blockSize);
+        _nodeTree->dataTransfer(loc[2],loc[1],_blockSize);
         loc+=3;
     }
     fprintf(stdout,"write op: ends at %lf",simtime());
@@ -59,17 +59,37 @@ void trafficManager::stripeOp(int* repLoc,int* ecLoc,int opNode){
     fprintf(stdout,"stripe op: starts at %lf",startTime);
     /* do download */
     for(int i=0;i<_ecK;i++){
-        ;
+        int* repPos=repLoc+i*_repFac;
+        int from=_nodeTree->getNearest(opNode,_repFac,repPos);
+        if(from!=-1){
+            _nodeTree->dataTransfer(opNode,from,_blockSize);
+        }else{
+            fprintf(stderr,"trafficManager::stripeOp(): weird thing happened\n");
+        }
     }
     /* Computation is much faster compared with download, so we do not consider 
       computation cost. */
     /* write parities */
     for(int i=_ecK;i<_ecN;i++){
-        ;
+        _nodeTree->dataTransfer(,opNode,_blockSize);
     }
-    /* re-allocation */
-    for(int i=0;i<_ecK;i++){
-        ;
+    /* re-allocation, if necessary */
+    if(_repPlaPolicy==0){
+        for(int i=0;i<_ecK;i++){
+            int* repPos=repLoc+i*_repFac;
+            int needRA=1;
+            for(int j=0;j<_repFac;j++){
+                if(ecLoc[i]==repPos[j]){
+                    needRA=0;
+                    break;
+                }
+            }
+            if(needRA==1){
+                _nodeTree->dataTransfer(ecLoc[i],
+                        _nodeTree->getNearest(ecLoc[i],_repFac,repPos),
+                        _blockSize);
+            }
+        }
     }
     fprintf(stdout,"stripe op: ends at %lf",simtime());
     return;
